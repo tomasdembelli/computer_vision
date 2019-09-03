70d94313990b03b4fd30ca0cfa8ac6955ead18f1
tomasdem <tomasdembelli@gmail.com>
2019-09-03 08:15:41 +0100

    Update: Argument parser added.

--- capture_cam.py
+++ capture_cam.py
@@ -13,26 +13,24 @@ class CaptureCam(object):
     """Capture video and save."""
 
     def __init__(self, cam_id=0, fourcc='XVID', path=os.getcwd(), 
-            file_name=None, res=(640, 480), color=True, fps=20):
+            file_name='Video.avi', res=(640, 480), color=True, fps=20):
         """Capture and store video from a camera."""
         self.cam_id = cam_id
         self.cap = cv.VideoCapture(self.cam_id)
         if not self.cap.isOpened():
             self.cap.open()
+        self.record_start = False
+        # Initiate necessary attributes for saving video.
+        if not file_name.endswith('.avi'):
+            file_name = file_name + '.avi'
+        self.file_path = os.path.join(path, file_name)
+        print(f'Video will be saved to {self.file_path}')
+        self.fourCC = cv.VideoWriter_fourcc(*fourcc)
+        self.fps = fps
+        self.res = res
+        self.color = color
+        self.out = False    # Default no recoding.
 
-        # If there is file_name, initiate neccessary identifiers.
-        if file_name:
-            if not file_name.endswith('.avi'):
-                file_name = file_name + '.avi'
-            self.file_path = os.path.join(path, file_name)
-            print(f'Video will be saved to {self.file_path}')
-            self.fourCC = cv.VideoWriter_fourcc(*fourcc)
-            self.fps = fps
-            self.res = res
-            self.color = color
-            self.record_start = False
-        else:
-            self.out = False
 
     def capture(self):
         """Capture video."""
@@ -41,13 +39,13 @@ class CaptureCam(object):
             if ret:    # Reading frame is successfull.
                 cv.imshow(f' Camera {self.cam_id} View', frame)
                 frame_display_duration = 1    # miliseconds
-                if cv.waitKey(frame_display_duration) == ord('q'):    # quit
+                k = cv.waitKey(frame_display_duration)
+                if k == ord('q'):    # quit
                     break
-                elif (cv.waitKey(frame_display_duration) == ord('s') and 
-                        self.file_path and not self.record_start):    
+                elif (k == ord('s') and not self.record_start):    
+                    self.record_start = True
                     self.out = cv.VideoWriter(self.file_path, self.fourCC, 
                         self.fps, self.res, self.color)
-                    self.record_start = True
                 if self.record_start:
                     self.out.write(frame)
             else:
@@ -62,7 +60,31 @@ class CaptureCam(object):
 
 
 if __name__ == "__main__":
-    session = CaptureCam(file_name='test')
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Capture videos.')
+    parser.add_argument('-c', '--cam', default=0, type=int,
+                        help='Camera id - default 0')
+    parser.add_argument('-4', '--fourc', default='XVID', type=str,
+                        help='Codec - default XVID')
+    parser.add_argument('-p', '--path', default=os.getcwd(),type=str,
+                        help='Folder to save camera record.')
+    parser.add_argument('-f', '--file', default='Video', type=str,
+                        help='Output file name with the extension.')
+    parser.add_argument('-x', '--res', default=(640, 480), type=tuple,
+                        help='Resolution - default (640, 480)')
+    parser.add_argument('-r', '--rgb', default=True, type=bool,
+                        help='Boolean - default True')
+    parser.add_argument('-@', '--fps', default=20, type=int,
+                        help='fps - default 20')
+    args = parser.parse_args()
+
+    #(self, cam_id=0, fourcc='XVID', path=os.getcwd(), file_name=None, res=(640, 480), color=True, fps=20):
+    
+    session = CaptureCam(cam_id=args.cam, fourcc=args.fourc, path=args.path, 
+                        file_name=args.file, res=args.res, color=args.rgb, 
+                        fps=args.fps)
     session.capture()
 
 
